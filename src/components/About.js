adce83285304067d2ef925b843e43207ac32b7e1
Timur <timursatarov3@gmail.com>
2023-08-27 19:12:00 +0200

    IMG

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import '../styles/about.css';
+import Avatar from '../images/avatar.png';
 
 const About = () => {
   return (
@@ -11,9 +12,16 @@ const About = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8 }}
     >
-      <div className="about-content">
-        <h2 className='about-me'>About Me</h2>
-        <p>My introduction goes here...</p>
+      <div className="about-container">
+        <div className="avatar-container">
+          <img src={Avatar} alt='Avatar' className="avatar" />
+        </div>
+        <div className="about-content">
+          <h2 className='about-me'>About Me</h2>
+          <p className='intro'>
+            I am a passionate Software Developer dedicated to continuous learning and improving my skillset to deliver efficient and high-quality results.
+          </p>
+        </div>
       </div>
     </motion.section>
   );
