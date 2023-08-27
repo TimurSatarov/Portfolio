01bc74e8f09781f101030067edff15148dc7a93b
Timur <timursatarov3@gmail.com>
2023-08-27 14:10:26 +0200

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState  } from 'react';
 import { motion } from 'framer-motion';
-import MemoryCardGame from './main-page/MemoryCardGame ';
+import Idle from '../gif/Idle.gif'
 import '../styles/home.css';
 
 const Home = () => {
@@ -11,12 +10,15 @@ const Home = () => {
       transition={{ duration: 0.3 }}
     > 
       <div className="home">
-        <div className="background-blur"></div>
         <div className="content">
           <h1 className="name">Timur Satarov</h1>
           <p className="role">Full-Stack Developer</p>
         </div>
+        <div className="gif-container">
+          <img src={Idle} alt="Idle character" className="gif" />
+        </div>
       </div>
+      <div className="background-blur"></div>
     </motion.section>
   );
 };
